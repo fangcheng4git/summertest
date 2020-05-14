9606cb204930a7aa55f1ef7369df369e46bf1cb0
fangc1 <fangc1@ziroom.com>
2020-05-14 09:16:12 +0800

    StopwatchTest学习

--- src/main/java/com/fangcheng/summertest/util/StopwatchTest.java
+++ src/main/java/com/fangcheng/summertest/util/StopwatchTest.java
@@ -1,8 +1,33 @@
 package com.fangcheng.summertest.util;
 
+import org.springframework.util.StopWatch;
+
 /**
+ * Stopwatch学习
  * @author fangcheng
  * @date 2020-05-14
  */
 public class StopwatchTest {
+
+
+    private static void springTest(){
+        StopWatch stopWatch = new StopWatch("1");
+        StopWatch stopWatch1 = new StopWatch("1");
+
+
+        System.out.println(stopWatch.getId());
+        System.out.println(stopWatch1.getId());
+    }
+
+
+    public static void main(String[] args) {
+        springTest();
+    }
+
+
+
+
+
+
+
 }
