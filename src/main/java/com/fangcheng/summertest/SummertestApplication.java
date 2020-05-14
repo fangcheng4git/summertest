3587d4fc12eb3de2373541cc37d6b61303eae101
fangc1 <fangc1@ziroom.com>
2020-05-14 08:01:13 +0800

    初始化提交

---
+++ src/main/java/com/fangcheng/summertest/SummertestApplication.java
@@ -0,0 +1,13 @@
+package com.fangcheng.summertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummertestApplication.class, args);
+    }
+
+}
